c24b2cac0111fc8d44ea8b3a0c5d0f3bc77c03c2
roy <roy.henning@gmx.net>
2011-01-20 23:04:34 +0100

    - Datensätze erweitert
    - Datentyp für die Artikelbeschreibung auf "Text" verändert
    - Produktbeschreibungen für 6 filme (chaos, bankjob, avatar, iron man 2, + noch was)
    - User Controller bearbeitet
    - DBCommands angepasst

--- src/Servlets/User.java
+++ src/Servlets/User.java
@@ -1,10 +1,15 @@
 package Servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import DBConnection.DBCommands;
+import MemberManagement.Member;
 
 /**
  * Servlet implementation class User
@@ -24,7 +29,34 @@ public class User extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String rsUserid = request.getParameter("userId");
+		String requestDestination ="";
+		
+		if (rsUserid != null){
+			Member m = DBCommands.SelectMemberByID(Integer.parseInt(rsUserid));
+			request.setAttribute("userId", m.GetMemberID());
+			request.setAttribute("fname", m.GetFirstName());
+			request.setAttribute("lname", m.GetLastName());
+			request.setAttribute("email", m.GetEMail());
+			request.setAttribute("zip", m.GetPostCode());
+			request.setAttribute("street", m.GetStreet());
+			request.setAttribute("hnr", m.GetStreetNumber());
+			request.setAttribute("place", m.GetCity());
+			requestDestination = "/userDataView.jsp";
+		}
+		else
+		{
+			requestDestination = "/Home";
+		}
+		
+//		
+//		request.setAttribute("object", m);
+//		request.setAttribute("userId", m.GetMemberID());
+//		request.setAttribute("userId", rsUserid);
+//		request.setAttribute("fname", "Roy");
+//		request.setAttribute("lname", "");
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(requestDestination);
+		dispatcher.forward(request, response);
 	}
 
 	/**
